2c49d571e4e3eae85c2d179797b3b212c5e2b643
FranciumSoftware <156525035+FranciumSoftware@users.noreply.github.com>
2024-01-20 10:32:00 +0100

    Add files via upload

--- update.js
+++ update.js
@@ -1,4 +1,13 @@
 var update=false;
-if(update==true){
-    alert("Mise à jour disponible");
+let version=1;
+let NewsVersion=2;
+document.getElementById('version').style.display='none';
+function verif(){
+    if(update===true){
+        document.getElementById('result').innerHTML = "Mise à jour disponible";
+        document.getElementById('version').style.display='block';
+        document.getElementById('version').innerHTML = "V2.0";
+    } else{
+        document.getElementById('result').innerHTML = "Jeux est à jour";
+    }
 }
